c9296d869395ac4fbc286708c0ab67995982a703
Jonathas Oliveira Conceição <jonholiveira18@hotmail.com>
2021-06-01 16:04:22 -0300

    commit inserindo php no arquivo index
    inseri código php no arquivo indez para exibir os dados do banco na tabela

--- includes/banco.php
+++ includes/banco.php
@@ -13,20 +13,4 @@
 	$banco->query("SET character_set_client=utf8");
 	$banco->query("SET character_set_results=utf8");
 	
-	//2º)Agora que conseguimos acessar o BD, podemos fazer requisições. Para isso, usaremos o método query() no objeto $banco
-	//No exemplo abaixo, estamos capturando TODOS OS registros da tabela GENEROS
-	$busca = $banco->query("SELECT * FROM USUARIOS");
-	if(!$busca){//Se houver erro durante a requisição
-		echo "Erro durante a requisição: $banco->error";
-	}else{
-		//3º)Para capturar CADA REGISTRO (linha) individualmente, usamos o método fetch_object()
-		//Cada vez que o comando $registro = $busca->fetch_object() é executado, UM registro é armazenado no objeto $registro. Assim, usamos o loop para capturar CADA UM DOS REGISTROS
-		while($registro = $busca->fetch_object()){
-			print_r($registro);
-			echo "<br>";
-		}
-	}
-	
-	
-	
 	
--- index.php
+++ index.php
@@ -6,14 +6,28 @@
 	<link rel="stylesheet" href="estilos/estilo.css"/>
 </head>
 <body>
+	<?php
+		require_once "includes/banco.php"
+	?>
 	<div id="corpo">
 		<h1>Escolha seu jogo</h1>
 		<table class="listagem">
-			<tr><td>Foto<td>Nome<td>Adm
-			<tr><td>Foto<td>Nome<td>Adm
-			<tr><td>Foto<td>Nome<td>Adm
-			<tr><td>Foto<td>Nome<td>Adm
+			<?php
+				$busca = $banco->query("SELECT * FROM JOGOS ORDER BY NOME");
+				if(!$busca){
+					echo "<p>Infelizmente a busca deu errado!</p>";
+				}else{
+					if($busca->num_rows == 0){
+						echo "<tr><td>Nenhum registro encontrado";
+					}else{
+						while($reg=$busca->fetch_object()){
+							echo "<tr><td>$reg->capa<td>$reg->nome<td>Adm";
+						}
+					}
+				}
+			?>
 		</table>
 	</div>
+	<?php $banco->close(); ?>
 </body>
 </html>
